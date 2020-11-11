a09dcad91f9f9190c1471efd123dc4171b540a0e
marksheekey <marksheekey@hotmail.com>
2020-11-11 21:22:42 +0000

    some decent stuff

--- components/SquadList.tsx
+++ components/SquadList.tsx
@@ -1,10 +1,10 @@
 import React, {Component} from "react";
-import {Text, View, FlatList} from "react-native";
-import { FloatingAction } from "react-native-floating-action";
+import {Text, View, FlatList, Button} from "react-native";
 import styles from "../styles/styles";
 import Dialog from 'react-native-dialog';
 
 class SquadList extends React.Component {
+
     state = {
         players: [
             {
@@ -21,35 +21,82 @@ class SquadList extends React.Component {
                 name: "Mark",
                 timePlayed: 0
             }
-        ]
+        ],
+        visible: false,
+        newPlayer: ""
     };
 
     render() {
         return (
             <View style = {styles.container}>
-                <FloatingAction onPressMain= { () => {
-                    <Dialog.Container
-                                 title={"DialogInput 1"}
-                                 message={"Message for DialogInput #1"}
-                                 hintInput ={"HINT INPUT"}
-                                 submitInput={ (inputText) => {this.sendInput(inputText)} }
-                                 closeDialog={ () => {this.showDialog(false)}}>
+                <View>
+                    <Dialog.Container visible={this.state.visible}>
+                        <Dialog.Title>Add Player</Dialog.Title>
+                        <Dialog.Button label="Add" onPress={() => {
+                            this.setState( {visible : false})
+                            console.log(this.state.newPlayer)
+                        }} />
+                        <Dialog.Button label="Cancel" onPress={ () => this.setState( {visible : false})  } />
+                        <Dialog.Input onChangeText={ value => this.setState( {newPlayer : value})  } />
+                        <Dialog.Description>
+                            Add a player to the squad
+                        </Dialog.Description>
+
                     </Dialog.Container>
-                }}/>
+                </View>
                 <FlatList data={this.state.players} renderItem={({ item }) => (
                     <Player
                         name={item.name}
                     />
-                )}
-                />
+                )}/>
+                <Button
+                    title="Add Player"
+                    onPress={() => {
+                        this.setState( {visible : true}) }}/>
+
             </View>
         );
     }
 
-
+    addPlayer = () => {
+        this.setState( {visible : false})
+        console.log(this.state.newPlayer)
+    }
 }
 
 export default SquadList
+
+
+class AddPlayer extends Component{
+    state = {
+        visible: true
+    }
+
+    render() {
+        console.log("addPlayer")
+        return (
+            <View>
+                <Dialog.Container visible={true}>
+                    <Dialog.Title>Account delete</Dialog.Title>
+                    <Dialog.Description>
+                        Add a player to the squad
+                    </Dialog.Description>
+
+                </Dialog.Container>
+            </View>
+        );
+    }
+}
+
+class PlayerType{
+    constructor(key, inMatch, pla)
+    key: "1",
+    inMatch: false,
+    playing: false,
+    name: "George",
+    timePlayed: 0
+}
+
 
 class Player extends Component<{ name: string }> {
     render() {
@@ -59,8 +106,4 @@ class Player extends Component<{ name: string }> {
             </View>
         );
     }
-}
-
-function addPlayer(){
-
 }
--- styles/styles.tsx
+++ styles/styles.tsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import { StyleSheet } from 'react-native';
 
+
 export default StyleSheet.create({
     container: {
         flex: 1,
